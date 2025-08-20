7466eb74e266c75f5204297f4f285cfce5be8788
Miguel Scatolin Teixeira <miguel.scatolin@gmail.com>
2025-08-19 21:02:59 -0300

    fix: terraform state moved to gcp bucket

--- main.tf
+++ main.tf
@@ -1,9 +1,26 @@
+provider "google" {
+  alias = "impersonation"
+  scopes = [
+    "https://www.googleapis.com/auth/cloud-platform",
+    "https://www.googleapis.com/auth/userinfo.email",
+  ]
+}
+
+
+#receive short-lived access token
+data "google_service_account_access_token" "default" {
+  provider               = google.impersonation
+  target_service_account = var.terraform_service_account
+  scopes                 = ["cloud-platform", "userinfo-email"]
+  lifetime               = "3600s"
+}
+
+
 terraform {
-  cloud {
-    organization = "scatolin-software"
-    workspaces {
-      name = "learn-terraform-migrate"
-    }
+  backend "gcs" {
+    bucket                      = "hermaeus-terraform-state"
+    prefix                      = "terraform/state"
+    impersonate_service_account = "terraform@hermaeus-466914.iam.gserviceaccount.com"
   }
 
   required_providers {
@@ -15,17 +32,12 @@ terraform {
 }
 
 provider "google" {
-  project = "hermaeus-466914"
-  region  = "us-central1"
-  zone    = "us-central1-c"
-  credentials = "./keys.json"
+  project      = "hermaeus-466914"
+  region       = "us-central1"
+  zone         = "us-central1-c"
+  access_token = data.google_service_account_access_token.default.access_token
 }
 
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
-
-
-
-
-
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "terraform_service_account" {
+  type        = string
+  description = "email adress of the service account used for terraform"
+
+}
+
+variable "project_id" {
+  type        = string
+  description = "ID of the project in scope"
+}
